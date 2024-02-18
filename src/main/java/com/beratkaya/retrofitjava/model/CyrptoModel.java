a9b8ab7134f30129cc888b257c4002c55045e011
BeratKaya28 <117393252+BeratKaya28@users.noreply.github.com>
2024-02-18 15:26:57 +0300

    Add files via upload
    Android Java Project

---
+++ src/main/java/com/beratkaya/retrofitjava/model/CyrptoModel.java
@@ -0,0 +1,13 @@
+package com.beratkaya.retrofitjava.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CyrptoModel {
+
+    @SerializedName("currency")
+   public String currency;
+   public @SerializedName("price")
+    String price;
+
+
+}
